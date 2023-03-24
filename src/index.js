25df6ecbbcd76d534384b7efc3a3340b1caa656b
iamnepster <jan-voelker@t-online.de>
2023-03-24 20:20:04 +0100

    update: postgres to sqlite3

--- src/index.js
+++ src/index.js
@@ -12,12 +12,10 @@ app.use(morgan("combined"));
 app.use(helmet());
 app.use(bodyParser.json());
 
-const postgres = new Sequelize(
-  "postgres://postgres:test123@localhost:31000/postgres"
-);
+const sequelize = new Sequelize("sqlite:./data/telemetry.db");
 
-const DhtLog = postgres.define("dht_log", {
-  clientid: DataTypes.STRING,
+const DhtLog = sequelize.define("dht_log", {
+  clientName: DataTypes.STRING,
   temperature: DataTypes.DOUBLE,
   humidity: DataTypes.DOUBLE,
   timestamp: DataTypes.DATE,
@@ -26,21 +24,23 @@ const DhtLog = postgres.define("dht_log", {
 const mqttClient = mqtt.connect("mqtt://localhost:1883");
 
 mqttClient.on("connect", () => {
-  mqttClient.subscribe("dht");
+  mqttClient.subscribe("climate");
 });
 
 mqttClient.on("message", async (_, message) => {
   await DhtLog.create(JSON.parse(message));
+  log.info(`Saved entry to queue ${JSON.parse(message)}`);
 });
 
-app.get("/api/dht", async (_, res) => {
+app.get("/api/climate", async (_, res) => {
   const dhtLogs = await DhtLog.findAll();
   res.send(dhtLogs);
 });
 
-app.post("/api/dht", async (req, res) => {
-  log.info(`Pushed entry to queue ${JSON.stringify(req.body)}`);
-  mqttClient.publish("dht", JSON.stringify(req.body));
+app.post("/api/climate", async (req, res) => {
+  const payload = JSON.stringify(req.body);
+  log.info(`Pushed entry to queue ${payload}`);
+  mqttClient.publish("climate", payload);
   res.status(202).send();
 });
 
@@ -48,8 +48,8 @@ app.listen(port, async () => {
   console.log(`Example app listening on port ${port}`);
 
   try {
-    await postgres.authenticate();
-    await postgres.sync();
+    await sequelize.authenticate();
+    await sequelize.sync();
     console.log("Connection has been established successfully.");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
