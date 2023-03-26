41011dca7808218a5698d3b2046a36b0310e3d9e
iamnepster <jan-voelker@t-online.de>
2023-03-26 22:58:19 +0200

    fix: mqtt subscription

--- src/mqtt/climate.mqtt.js
+++ src/mqtt/climate.mqtt.js
@@ -7,8 +7,9 @@ mqttClient.on("connect", () => {
   log.info(`Mqtt client successfully subscribed to topic [climate]`);
 });
 
-mqttClient.on("message", async (_, message) => {
-  log.debug(_);
-  await climateEntity.create(JSON.parse(message));
-  log.info(`Successfully saved entry to database ${message}`);
+mqttClient.on("message", async (topic, message) => {
+  if (topic === "climate") {
+    await climateEntity.create(JSON.parse(message));
+    log.info(`Successfully saved entry to database ${message}`);
+  }
 });
